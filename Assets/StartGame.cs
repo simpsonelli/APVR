c1e00d69c2aec209e93744ef4308978da46cc5f5
Michael Simpsonelli <83802317+simpsonelli@users.noreply.github.com>
2022-07-19 23:52:50 -0700

    Button_Function
    - Button works to enter game

--- Assets/StartGame.cs
+++ Assets/StartGame.cs
@@ -5,8 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
-    public void OnButtonClicked()
+   public void JustStart()
     {
         SceneManager.LoadScene("Gallery");
     }
+
+   
 }
